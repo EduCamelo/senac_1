5d2c9965b73919afc376cefebb1a7a65dc31fcdc
EduCamelo <eduardocamelobs@gmail.com>
2022-10-16 17:17:26 -0300

    senac_1

--- gallery/views.py
+++ gallery/views.py
@@ -5,3 +5,18 @@ def index(request):
 
 def imagem(request):
     return render(request,'gallery/imagem.html')
+
+def imagem2(request):
+    return render(request,'gallery/imagem2.html')
+
+def imagem3(request):
+    return render(request,'gallery/imagem3.html')
+
+def imagem4(request):
+    return render(request,'gallery/imagem4.html')
+
+def imagem5(request):
+    return render(request,'gallery/imagem5.html')
+
+def imagem6(request):
+    return render(request,'gallery/imagem6.html')
